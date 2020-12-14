ad47473aec835c1c8ccefc3ba92bd84fc186b8d7
d-hamasatp <dafubangli@gmail.com>
2020-12-14 18:00:28 +0900

    [js-2] finish

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,6 +2,45 @@
 
 // alert(hello);
 
-var int1 = 1;
+// var int1 = 1;
 
-alert(int1);
+// var str1 = "Hello";
+// var str2 = "World!!";
+
+// alert(str1 + str2);
+
+// var orange = 120;
+// var apple = 100;
+
+// if(orange < apple){
+//   alert("みかんの値段がリンゴより安い");
+// } else if(orange == apple){
+//   alert("みかんとリンゴが同じ値段");
+// } else{
+//   alert("みかんの値段がリンゴより高い");
+// }
+
+// var max = 200;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count +1;
+// }
+
+// alert("2を掛け続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert("1から10まで足算した結果は" + num + "です");
+
+var int1 = 9
+var int2 = 2
+
+alert(int1 % int2)
