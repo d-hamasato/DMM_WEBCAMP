9788679d3d8e10797fbff555f2f82abd223bb064
d-hamasatp <dafubangli@gmail.com>
2020-12-15 17:26:16 +0900

    [js-6] finish

--- JavaScript/jQuery-Lesson/app.js
+++ JavaScript/jQuery-Lesson/app.js
@@ -23,11 +23,41 @@
 // });
 
 // [js-5]確認問題
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'height': '100px'
+//     }).slideUp(1000);
+//   });
+// });
+
+
+// [js-6]
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'height': '100px'
-    }).slideUp(1000);
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
